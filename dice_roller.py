4cc7387c9b042f4e09534f9060b09ce591f7b0b1
Ula <ula@MacBook-Pro-Ula.local>
2021-11-08 14:34:28 +0100

    Adding some players

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,22 @@
 def main():
     import random
-    dice_rolls = 2
-    dice_sum = 0
-    for i in range(0,dice_rolls):
-        roll = random.randint(1,6)
-        dice_sum += 1
-        if roll == 1:
-            print(f'You rolled {roll} ! Critical Fail')
-        elif roll == 6:
-            print(f'You rolled {roll} ! Critical Success!')
-        else:
-            print(f'You rolled {roll}! Try harder!')
-    print(f'You have rolled a total of {dice_sum}')
+    results = {}
+
+    number_of_players = int(input('What is a number of players?'))
+    for i in range(number_of_players):
+        name = input('Give a player\'s name')
+        results[name] = {'total': 0, 'rolls': []}
+                            
+    dice_rolls = int(input('How many times will each playes roll a dice?'))                            
+    for key in results:
+        dice_sum = 0
+        for i in range(0,dice_rolls):
+            roll = random.randint(1,6)
+            results[key]['rolls'].append(roll)
+            dice_sum += roll
+        results[key]['total'] = dice_sum
+
+    print(f'Results {results}')
 
 if __name__== "__main__":
     main()
